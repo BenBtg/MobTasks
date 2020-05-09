52537fd1733a8ca5d3d4d13b261f2e593a9b7fa2
Ben Butttigieg <ben.buttigieg@microsoft.com>
2020-05-09 14:08:53 +0100

    Initial commit

---
+++ MobTasks/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MobTasks.Models;
+
+namespace MobTasks.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
